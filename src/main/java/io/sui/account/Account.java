8a4d6d1017eb383e14f046b6ca71cd6280a03212
fearlessfe <505380967@qq.com>
2023-02-07 22:01:07 +0800

    feat: fix typ

--- src/main/java/io/sui/account/Account.java
+++ src/main/java/io/sui/account/Account.java
@@ -1,18 +1,29 @@
+/*
+ * Copyright 2023 281165273grape@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package io.sui.account;
+
 
 import io.sui.crypto.ED25519KeyPair;
 import io.sui.crypto.SECP256K1KeyPair;
-import io.sui.crypto.SignatureSchemeNotSupportedException;
 import io.sui.crypto.SuiKeyPair;
-import org.apache.commons.lang3.StringUtils;
+import java.util.List;
 import org.bitcoinj.crypto.*;
 import org.bouncycastle.crypto.params.Ed25519PrivateKeyParameters;
 import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
-
-import java.security.SecureRandom;
-import java.util.Arrays;
-import java.util.List;
-
 
 // https://github.com/satoshilabs/slips/blob/master/slip-0010.md
 // https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki
@@ -20,54 +31,49 @@ import java.util.List;
 // https://github.com/MystenLabs/sui
 // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
 
-
 public class Account {
 
-    private List<String> wordList;
-    private String mnemonic;
-    private byte[] seed;
-    private SuiKeyPair<?> keyPair;
+  private List<String> wordList;
+  private String mnemonic;
+  private byte[] seed;
+  private SuiKeyPair<?> keyPair;
 
-    private AccountType accountType;
+  private AccountType accountType;
 
-    public Account(AccountType accountType) {
-        this.accountType = accountType;
-        this.wordList = Mnemonic.generateMnemonic();
-        this.mnemonic = String.join(" ", wordList);
-        this.seed = Mnemonic.toSeed(wordList, "");
+  public Account(AccountType accountType) {
+    this.accountType = accountType;
+    this.wordList = Mnemonic.generateMnemonic();
+    this.mnemonic = String.join(" ", wordList);
+    this.seed = Mnemonic.toSeed(wordList, "");
+  }
+
+  private SuiKeyPair<?> genKeyPair() {
+    switch (this.accountType) {
+      case ED25519:
+        return genED25519KeyPair();
+      case Secp256k1:
+        return genSECP256K1KeyPair();
+      default:
+        return null;
+        //                throw new SignatureSchemeNotSupportedException();
     }
+  }
 
+  private ED25519KeyPair genED25519KeyPair() {
+    ED25519DeterministicKey key = ED25519DeterministicKey.createKeyByDefaultPath(this.seed);
+    Ed25519PrivateKeyParameters parameters = new Ed25519PrivateKeyParameters(key.getKey());
+    Ed25519PublicKeyParameters publicKeyParameters = parameters.generatePublicKey();
 
-    private SuiKeyPair<?> genKeyPair() {
-        switch (this.accountType) {
-            case ED25519:
-                return genED25519KeyPair();
-            case Secp256k1:
-                return genSECP256K1KeyPair();
-            default:
-                return null;
-//                throw new SignatureSchemeNotSupportedException();
-        }
-    }
+    ED25519KeyPair keyPair = new ED25519KeyPair(parameters, publicKeyParameters);
+    this.keyPair = keyPair;
+    return keyPair;
+  }
 
-    private ED25519KeyPair genED25519KeyPair() {
-       ED25519DeterministicKey key = ED25519DeterministicKey.createKeyByDefaultPath(this.seed);
-       Ed25519PrivateKeyParameters parameters = new Ed25519PrivateKeyParameters(key.getKey());
-       Ed25519PublicKeyParameters publicKeyParameters = parameters.generatePublicKey();
+  private SECP256K1KeyPair genSECP256K1KeyPair() {
+    SECP256K1DeterministicKey key = SECP256K1DeterministicKey.createKeyByDefaultPath(this.seed);
 
-       ED25519KeyPair keyPair = new ED25519KeyPair(parameters, publicKeyParameters);
-       this.keyPair = keyPair;
-       return keyPair;
-    }
-
-    private SECP256K1KeyPair genSECP256K1KeyPair() {
-        SECP256K1DeterministicKey key = SECP256K1DeterministicKey.createKeyByDefaultPath(this.seed);
-
-        SECP256K1KeyPair keyPair = new SECP256K1KeyPair(key.getKey());
-        this.keyPair = keyPair;
-        return keyPair;
-    }
-
-
-
+    SECP256K1KeyPair keyPair = new SECP256K1KeyPair(key.getKey());
+    this.keyPair = keyPair;
+    return keyPair;
+  }
 }
--- src/main/java/io/sui/account/ED25519DeterministicKey.java
+++ src/main/java/io/sui/account/ED25519DeterministicKey.java
@@ -1,86 +1,98 @@
+/*
+ * Copyright 2023 281165273grape@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package io.sui.account;
 
+
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Iterator;
 import org.apache.commons.lang3.StringUtils;
 import org.bitcoinj.crypto.ChildNumber;
 import org.bitcoinj.crypto.HDPath;
 import org.bitcoinj.crypto.HDUtils;
 
-import java.io.ByteArrayOutputStream;
-import java.math.BigInteger;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
-import java.util.Iterator;
-
 public class ED25519DeterministicKey {
 
-    private final String defaultDerivePath = "m/44H/784H/0H/0H/0H";
-    private byte[] key;
-    private byte[] chaincode;
+  private final String defaultDerivePath = "m/44H/784H/0H/0H/0H";
+  private byte[] key;
+  private byte[] chaincode;
 
-    public static ED25519DeterministicKey createKeyByDefaultPath(byte[] seed) {
-        return createMasterKey(seed).deriveFromPath("");
+  public static ED25519DeterministicKey createKeyByDefaultPath(byte[] seed) {
+    return createMasterKey(seed).deriveFromPath("");
+  }
+
+  public static ED25519DeterministicKey createMasterKey(byte[] seed) {
+    byte[] i = HDUtils.hmacSha512("ed25519 seed".getBytes(), seed);
+    byte[] il = Arrays.copyOfRange(i, 0, 32);
+    byte[] ir = Arrays.copyOfRange(i, 32, 64);
+    return new ED25519DeterministicKey(il, ir);
+  }
+
+  public ED25519DeterministicKey(byte[] key, byte[] chaincode) {
+    this.key = key;
+    this.chaincode = chaincode;
+  }
+
+  public ED25519DeterministicKey derive(int index) throws Exception {
+    if (!hasHardenedBit(index)) {}
+
+    byte[] indexBytes = new byte[4];
+    ByteBuffer.wrap(indexBytes).putInt(index);
+
+    byte[] a = new byte[] {0x00};
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    outputStream.write(a);
+    outputStream.write(this.key);
+    outputStream.write(indexBytes);
+
+    byte[] data = outputStream.toByteArray();
+
+    outputStream.write(a);
+    byte[] i = HDUtils.hmacSha512(this.chaincode, data);
+    byte[] il = Arrays.copyOfRange(i, 0, 32);
+    byte[] ir = Arrays.copyOfRange(i, 32, 64);
+
+    return new ED25519DeterministicKey(il, ir);
+  }
+
+  public ED25519DeterministicKey deriveFromPath(String path) {
+    if (StringUtils.isAnyBlank(path)) {
+      path = defaultDerivePath;
     }
-
-    public static ED25519DeterministicKey createMasterKey(byte[] seed) {
-        byte[] i = HDUtils.hmacSha512("ed25519 seed".getBytes(), seed);
-        byte[] il = Arrays.copyOfRange(i, 0, 32);
-        byte[] ir = Arrays.copyOfRange(i, 32, 64);
-        return new ED25519DeterministicKey(il, ir);
+    HDPath hdPath = HDPath.parsePath(path);
+    Iterator<ChildNumber> it = hdPath.iterator();
+    ED25519DeterministicKey current = this;
+    while (it.hasNext()) {
+      current = current.derive(it.next().getI());
     }
+    return current;
+  }
 
-    public ED25519DeterministicKey(byte[] key, byte[] chaincode) {
-        this.key = key;
-        this.chaincode = chaincode;
-    }
+  private boolean hasHardenedBit(int a) {
+    return (a & ChildNumber.HARDENED_BIT) != 0;
+  }
 
-    public ED25519DeterministicKey derive(int index) throws Exception {
-        if (!hasHardenedBit(index)) {
+  public byte[] getKey() {
+    return key;
+  }
 
-        }
-
-        byte[] indexBytes = new byte[4];
-        ByteBuffer.wrap(indexBytes).putInt(index);
-
-        byte[] a = new byte[]{0x00};
-
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        outputStream.write(a);
-        outputStream.write(this.key);
-        outputStream.write(indexBytes);
-
-        byte[] data = outputStream.toByteArray();
-
-        outputStream.write(a);
-        byte[] i = HDUtils.hmacSha512(this.chaincode, data);
-        byte[] il = Arrays.copyOfRange(i, 0, 32);
-        byte[] ir = Arrays.copyOfRange(i, 32, 64);
-
-        return new ED25519DeterministicKey(il, ir);
-    }
-
-    public ED25519DeterministicKey deriveFromPath(String path) {
-        if (StringUtils.isAnyBlank(path)) {
-            path = defaultDerivePath;
-        }
-        HDPath hdPath = HDPath.parsePath(path);
-        Iterator<ChildNumber> it = hdPath.iterator();
-        ED25519DeterministicKey current = this;
-        while (it.hasNext()) {
-            current = current.derive(it.next().getI());
-        }
-        return current;
-    }
-
-    private boolean hasHardenedBit(int a) {
-        return (a & ChildNumber.HARDENED_BIT) != 0;
-    }
-
-    public byte[] getKey() {
-        return key;
-    }
-
-    public byte[] getChaincode() {
-        return chaincode;
-    }
-
+  public byte[] getChaincode() {
+    return chaincode;
+  }
 }
